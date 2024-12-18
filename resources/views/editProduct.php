aac19fdb415d1f8db6bb63256f6c2fc780595620
ArdenDiMarunongMagCode <ardenkylde.deocampo@gmail.com>
2024-12-18 12:40:38 +0800

    for edit of products

---
+++ resources/views/editProduct.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once("../app/Controller/ProductController.php");
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Check if the user is logged in and has admin privileges
+if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true || $_SESSION['role'] !== 'admin') {
+    // echo '<h1>Access Denied</h1>';
+    // echo '<p>You do not have permission to view this page. Please contact your administrator if you believe this is an error.</p>';
+    $alertMessage = "You do not have permission to view this page. Please contact your administrator if you believe this is an error.";
+    include_once('../resources/components/dangerAlert.php');
+    exit;
+}
+
+$message = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $productController = new ProductController();
+    $productController->createProduct($_POST);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Product</title>
+    <link rel="stylesheet" href="css/adminStyles.css">
+</head>
+
+<body>
+    <?php
+    require_once __DIR__ . '/../components/adminHeader.php';
+
+
+    require_once __DIR__ . '/../components/editProductForm.php';
+    ?>
+</body>
+
+</html>
